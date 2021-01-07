b29fc24407d9ee839ae0e7a9d4aec615e4bbb006
Perkins <1165459287@qq.com>
2021-01-07 10:11:25 +0800

    feat:增加使用说明

--- src/main/java/com/perkins/Demo/service/LogServiceImpl.java
+++ src/main/java/com/perkins/Demo/service/LogServiceImpl.java
@@ -1,6 +1,9 @@
 package com.perkins.Demo.service;
 
 import com.perkins.simplelog.annotation.SimpleLog;
+import com.perkins.simplelog.common.LoggerUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,9 +11,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LogServiceImpl {
+    private static final Logger logger = LogManager.getLogger(LogServiceImpl.class);
 
     @SimpleLog
     public String setName(String name) {
         return name;
+    }
+
+    public String getList(){
+        LoggerUtil.info(logger, "这是一个自定义日志");
+        return null;
     }
 }
